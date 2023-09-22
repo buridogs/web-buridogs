e026a59b9ee8e47191f8bcf7e93c8acd4fd67498
Diogo Almazan <diogodpa@outlook.com>
2023-09-21 22:08:32 -0300

    teste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
-import { ReactNode } from "react";
+import { ReactNode, AnchorHTMLAttributes } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Header } from "@/components/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <Header />
+                {children}
+            </body>
         </html>
     );
 }
