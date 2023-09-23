7a9202e34422d2adbee65ea33c9fd5cbddf2db37
Diogo Almazan <diogodpa@outlook.com>
2023-09-23 15:26:17 -0300

    Header and Header mobile implemented

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,22 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { HeaderMobile } from "./HeaderMobile";
+import { headerMenuLink } from "./utils";
 
 export function Header() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const pathname = usePathname();
+
+    useEffect(() => {
+        setIsNavOpen(false);
+    }, [pathname]);
+
     return (
-        <div className="w-[100%] bg-white py-8">
+        <div className="w-[100%] bg-white py-8 px-8 xl:px-0">
             <section className="max-w-screen-xl flex justify-between items-center m-auto">
                 <Link href="/">
                     <Image
@@ -15,11 +28,23 @@ export function Header() {
                     />
                 </Link>
                 <nav>
-                    <Link href="/adocao">Adoção</Link>
-                    <Link href="/finais-felizes">Finais Felizes</Link>
-                    <Link href="/parceiros">Parceiros</Link>
-                    <Link href="/sobre-nos">Sobre Nós</Link>
-                    <Link href="/contato">Contato</Link>
+                    <HeaderMobile
+                        isOpen={isNavOpen}
+                        setIsOpen={setIsNavOpen}
+                    />
+
+                    <ul className="hidden space-x-8 lg:flex">
+                        {headerMenuLink.map((menuOption) => (
+                            <li key={menuOption.label}>
+                                <Link
+                                    href={menuOption.path}
+                                    className="text-grey-700 transition-color ease-in-out delay-800 hover:text-primary-400"
+                                >
+                                    {menuOption.label}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
                 </nav>
             </section>
         </div>
