7006223581d31ada3e141b34fcb8a540404f130a
Diogo Almazan <diogodpa@outlook.com>
2023-12-05 22:24:06 -0300

    Add react hook form and implemented adoption form structure

--- src/components/app/adocaoDetalhes/AdocaoDetalhesForm.tsx
+++ src/components/app/adocaoDetalhes/AdocaoDetalhesForm.tsx
@@ -1,10 +1,147 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { ADOCAO_FORMS_CONFIG, schemaAdocaoForm } from "./AdocaoDetalhesUtils";
+import { IAdocaoForm } from "./AdocaoDetalhesTypes";
+
 export default function AdocaoDetalhesForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IAdocaoForm>({
+        resolver: yupResolver(schemaAdocaoForm),
+    });
+    const onSubmit = (data) => console.log(data);
+
     return (
         <section className="flex flex-col items-center">
             <h2 className="text-primary-400 text-2xl font-bold">
                 Quer adotar? Preencha o formulário abaixo e entraremos em contato com você!
             </h2>
-            <div>Forms</div>
+            <div className="flex flex-col">
+                <span className="text-grey-400 mt-4 mb-4">
+                    Nós, protetores, realizamos essa entrevista para conhecer um pouco mais sobre
+                    o(s) interessado(s) em adotar um cão resgatado que foi tirado das ruas, saber
+                    sobre os motivos da adoção, sobre o ambiente no qual o bichinho vai morar e
+                    sobre experiências prévias que eles possam já ter tido com outros animais. Após
+                    a entrevista,{" "}
+                    <strong className="text-grey-700">
+                        sendo aprovada a adoção pelas protetoras
+                    </strong>
+                    , será formalizado um Termo de Adoção e Responsabilidades. Neste termo estarão
+                    as diretrizes básicas de cuidados com o animal, os dados completos do adotante e
+                    do protetor tutor (endereço, telefone e e-mail de contato, nome completo e
+                    documentos), assinado por ambos, de forma a garantir o bem estar do animalzinho
+                    adotado.
+                </span>
+                <p className="text-grey-400 mb-4">
+                    Após a adoção,{" "}
+                    <strong className="text-grey-700">
+                        os protetores irão esporadicamente monitorar as condições de adaptação e
+                        vida do animal, até que todos estejam completamente adaptados e felizes.
+                    </strong>{" "}
+                    Por gentileza, responda as perguntas abaixo. Assim que nos enviar, iremos
+                    analisar e responder o mais breve possível. Sendo aprovada a adoção, marcamos
+                    uma visita para que o interessado conheça o animal e interaja com ele antes que
+                    seja levado ao adotante.
+                </p>
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="flex flex-col items-end"
+                >
+                    {ADOCAO_FORMS_CONFIG.map((adocaoKey) => (
+                        <div
+                            key={adocaoKey.section[0].key}
+                            className="w-full flex flex-col items-center justify-around md:flex-row"
+                        >
+                            {adocaoKey.section.map((field) => (
+                                <div
+                                    key={field.key}
+                                    className="w-full flex flex-col items-start mt-4"
+                                >
+                                    <label
+                                        htmlFor={field.key}
+                                        className="text-sm text-grey-100 font-medium"
+                                    >
+                                        {field.label}
+                                    </label>
+                                    {field.type === "text" ? (
+                                        <input
+                                            id={field.key}
+                                            placeholder={field.placeholder ?? ""}
+                                            className="w-[80%] py-2 px-2 border-2 border-grey-100 border-solid rounded mt-1"
+                                            {...register(field.key as keyof IAdocaoForm)}
+                                        />
+                                    ) : field.type === "textarea" ? (
+                                        <textarea
+                                            id={field.key}
+                                            placeholder={field.placeholder ?? ""}
+                                            className="w-[80%] py-2 px-2 border-2 border-grey-100 border-solid rounded mt-1"
+                                            {...register(field.key as keyof IAdocaoForm)}
+                                            rows={4}
+                                        />
+                                    ) : field.type === "radio" ? (
+                                        field.options?.map((opt) => (
+                                            <div
+                                                key={opt.key}
+                                                className="w-full flex items-center justify-start"
+                                            >
+                                                <input
+                                                    id={opt.key}
+                                                    type="radio"
+                                                    value={opt.value}
+                                                    className="mr-2"
+                                                    {...register(field.key as keyof IAdocaoForm)}
+                                                />
+                                                <label
+                                                    htmlFor={opt.key}
+                                                    className="w-full text-sm text-grey-100 font-normal"
+                                                >
+                                                    {opt.label}
+                                                </label>
+                                            </div>
+                                        ))
+                                    ) : field.type === "checkbox" ? (
+                                        field.options?.map((opt) => (
+                                            <div
+                                                key={opt.key}
+                                                className="w-full flex items-center justify-start"
+                                            >
+                                                <input
+                                                    id={opt.key}
+                                                    type="checkbox"
+                                                    value={opt.value}
+                                                    className="mr-2"
+                                                    {...register(field.key as keyof IAdocaoForm)}
+                                                />
+                                                <label
+                                                    htmlFor={opt.key}
+                                                    className="w-full text-sm text-grey-100 font-normal"
+                                                >
+                                                    {opt.label}
+                                                </label>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <></>
+                                    )}
+                                    <p className="text-sm font-semibold text-red-400">
+                                        {errors[field.key as keyof IAdocaoForm]?.message}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+
+                    <input
+                        type="submit"
+                        value="Enviar forms"
+                        className="uppercase font-medium py-2 px-4 rounded border-grey-400 border-solid border-2 mt-8 cursor-pointer transition duration-150 hover:bg-primary-100 hover:text-white hover:border-primary-100"
+                    />
+                </form>
+            </div>
         </section>
     );
 }
---
+++ src/components/app/adocaoDetalhes/AdocaoDetalhesUtils.ts
@@ -0,0 +1,307 @@
+import { AdocaoFormsType, InputFormEnum } from "./AdocaoDetalhesTypes";
+import * as yup from "yup";
+
+export const ADOCAO_FORMS_CONFIG: AdocaoFormsType[] = [
+    {
+        section: [
+            {
+                key: "nome",
+                label: "Nome responsável",
+                placeholder: "Exemplo: José da Silva",
+                type: InputFormEnum.text,
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "endereco_cep",
+                label: "CEP",
+                placeholder: "Formato XX.XXX-XXX",
+                type: InputFormEnum.text,
+            },
+            {
+                key: "endereco_rua",
+                label: "Logradouro",
+                placeholder: "Exemplo: Avenida Mário Werneck",
+                type: InputFormEnum.text,
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "endereco_numero",
+                label: "Número",
+                placeholder: "Exemplo: 365",
+                type: InputFormEnum.text,
+            },
+            {
+                key: "endereco_complemento",
+                label: "Complemento",
+                placeholder: "Exemplo: Esquina",
+                type: InputFormEnum.text,
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "endereco_bairro",
+                label: "Bairro",
+                placeholder: "Exemplo: Centro",
+                type: InputFormEnum.text,
+            },
+            {
+                key: "endereco_cidade",
+                label: "Cidade",
+                placeholder: "Exemplo: Belo Horizonte",
+                type: InputFormEnum.text,
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "endereco_estado",
+                label: "Estado",
+                placeholder: "Exemplo: Minas Gerais",
+                type: InputFormEnum.text,
+            },
+        ],
+    },
+    {
+        section: [
+            { key: "facebook_url", label: "Facebook Link", type: InputFormEnum.text },
+            { key: "instagram_url", label: "Instagram Link", type: InputFormEnum.text },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "primeira_adocao",
+                label: "Primeira adoção?",
+                type: InputFormEnum.radio,
+                options: [
+                    {
+                        key: "primeira_adocao",
+                        label: "Sim, é a primeira",
+                        value: 1,
+                    },
+                    {
+                        key: "adotou_antes",
+                        label: "Já adotei antes",
+                        value: 0,
+                    },
+                ],
+            },
+            {
+                key: "motivo_adocao",
+                label: "Motivo adoção",
+                placeholder: "Exemplo: Para me fazer companhia",
+                type: InputFormEnum.text,
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "quantidade_pessoas_moradia",
+                label: "Quantas pessoas moram?",
+                placeholder: "Exemplo: 3 pessoas",
+                type: InputFormEnum.text,
+            },
+            {
+                key: "pessoas_de_acordo_adocao",
+                label: "As pessoas estão de acordo?",
+                type: InputFormEnum.radio,
+                options: [
+                    {
+                        key: "de_acordo",
+                        label: "De acordo",
+                        value: 1,
+                    },
+                    {
+                        key: "desacordo",
+                        label: "Não sabem ou não estão de acordo",
+                        value: 0,
+                    },
+                ],
+            },
+            {
+                key: "ha_criancas_idosos",
+                label: "Há crianças ou idosos?",
+                type: InputFormEnum.checkbox,
+                options: [
+                    {
+                        key: "criancas",
+                        label: "Há crianças",
+                        value: "criancas",
+                    },
+                    {
+                        key: "idosos",
+                        label: "Há idosos",
+                        value: "idosos",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "mora_casa_apt",
+                label: "Mora em casa ou apartamento?",
+                type: InputFormEnum.radio,
+                options: [
+                    {
+                        key: "casa",
+                        label: "Moro em casa",
+                        value: "Casa",
+                    },
+                    {
+                        key: "apt",
+                        label: "Moro em apartamento",
+                        value: "Apartamento",
+                    },
+                ],
+            },
+            {
+                key: "quantidade_pessoas_trabalham",
+                label: "Quantas pessoas trabalham?",
+                placeholder: "Somente eu",
+                type: InputFormEnum.text,
+            },
+            {
+                key: "moradia_tem_estrutura_adocao",
+                label: "O lugar tem estrutura para acolher o animal?",
+                type: InputFormEnum.radio,
+                options: [
+                    {
+                        key: "tem_estrutura",
+                        label: "Tem estrutura",
+                        value: "O lugar tem estrutura",
+                    },
+                    {
+                        key: "nao_tem_estrutura",
+                        label: "Não tem estrutura",
+                        value: "Não tem estrutura",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "ha_outros_animais",
+                label: "Você possui outros animais? Se sim, quantos?",
+                placeholder: "Exemplo: Há outros 3 animais",
+                type: InputFormEnum.text,
+            },
+            {
+                key: "ja_teve_outros_animais",
+                label: "Já teve outros animais?",
+                type: InputFormEnum.radio,
+                options: [
+                    {
+                        key: "sim",
+                        label: "Já tive",
+                        value: "Já tive",
+                    },
+                    {
+                        key: "nao",
+                        label: "Nunca tive um animal",
+                        value: "Nunca tive um animal",
+                    },
+                ],
+            },
+            {
+                key: "esta_ciente_gastos",
+                label: "Você está ciente dos gastos?",
+                type: InputFormEnum.radio,
+                options: [
+                    {
+                        key: "ciente",
+                        label: "Estou ciente dos gastos",
+                        value: 1,
+                    },
+                    {
+                        key: "nao_ciente",
+                        label: "Não estou ciente",
+                        value: 0,
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "descricao_lugar_animal",
+                label: "Descreva o lugar que o animal ficará",
+                placeholder: "Exemplo: Apartamento possui tela de proteção",
+                type: InputFormEnum.textarea,
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "situacao_devolucao_adocao",
+                label: "Pensa em devolver o animal em alguma situação?",
+                type: InputFormEnum.textarea,
+            },
+        ],
+    },
+    {
+        section: [
+            {
+                key: "consciente_termo_responsabilidade",
+                label: "Está ciente em assinar o Termo de Responsabilidade?",
+                type: InputFormEnum.radio,
+                options: [
+                    {
+                        key: "estou_ciente",
+                        label: "Estou ciente",
+                        value: 1,
+                    },
+                    {
+                        key: "nao_estou_ciente",
+                        label: "Não estou ciente",
+                        value: 0,
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+export const schemaAdocaoForm = yup
+    .object({
+        nome: yup.string().required("Campo Obrigatório"),
+        endereco_cep: yup.string().required("Campo Obrigatório"),
+        endereco_rua: yup.string().required("Campo Obrigatório"),
+        endereco_numero: yup.string().required("Campo Obrigatório"),
+        endereco_complemento: yup.string(),
+        endereco_bairro: yup.string().required("Campo Obrigatório"),
+        endereco_cidade: yup.string().required("Campo Obrigatório"),
+        endereco_estado: yup.string().required("Campo Obrigatório"),
+        facebook_url: yup.string().required("Campo Obrigatório"),
+        instagram_url: yup.string().required("Campo Obrigatório"),
+        primeira_adocao: yup.boolean().required("Campo Obrigatório"),
+        motivo_adocao: yup.string().required("Campo Obrigatório"),
+        quantidade_pessoas_moradia: yup.string().required("Campo Obrigatório"),
+        pessoas_de_acordo_adocao: yup.boolean().required("Campo Obrigatório"),
+        ha_criancas_idosos: yup.string().optional(),
+        mora_casa_apt: yup.string().required("Campo Obrigatório"),
+        quantidade_pessoas_trabalham: yup.string().required("Campo Obrigatório"),
+        moradia_tem_estrutura_adocao: yup.string().required("Campo Obrigatório"),
+        ha_outros_animais: yup.string().required("Campo Obrigatório"),
+        ja_teve_outros_animais: yup.string().required("Campo Obrigatório"),
+        esta_ciente_gastos: yup.boolean().required("Campo Obrigatório"),
+        descricao_lugar_animal: yup.string().required("Campo Obrigatório"),
+        situacao_devolucao_adocao: yup.string().required("Campo Obrigatório"),
+        consciente_termo_responsabilidade: yup.boolean().required("Campo Obrigatório"),
+    })
+    .required();
