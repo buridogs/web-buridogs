f20635444eccf337a76374610916ff6163e26698
joaolotjr <joaolotjr@gmail.com>
2023-12-22 14:36:43 -0300

    Criação da função de enviar email para pagina contato

--- src/components/app/contato/ContatoDetalhesForm.tsx
+++ src/components/app/contato/ContatoDetalhesForm.tsx
@@ -3,19 +3,31 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { CONTATO_FORMS_CONFIG, schemaContatoForm } from "./ContatoUtils";
 import { IContatoFormData } from "./ContatoTypes";
+import { sendEmailFunctionContato } from "@/services/azure-function/send-email-contato/send-email-contato-api";
+import { toast } from "react-toastify";
 
 export function ContatoDetalhesForm() {
     const {
         register,
         handleSubmit,
         formState: { errors },
+        reset,
     } = useForm<IContatoFormData>({
         resolver: yupResolver(schemaContatoForm),
     });
 
-    const onSubmit = (data: IContatoFormData) => {
-        alert(JSON.stringify(data));
-        console.log(data);
+    const onSubmit = async (data: IContatoFormData) => {
+        try {
+            await sendEmailFunctionContato({
+                ...data,
+            });
+            toast.success("Formulário enviado com sucesso!");
+        } catch (err: any) {
+            console.warn(err.message);
+            toast.error("Houve um erro no envio do formulário");
+        } finally {
+            reset();
+        }
     };
 
     return (
