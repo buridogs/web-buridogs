6a330469dff441d534ec846cd3f4a8ef02faa945
Diogo Almazan <diogodpa@outlook.com>
2023-12-22 13:09:29 -0300

    Filled Sobre Nos page and separated data into mock folder

---
+++ src/mock/sobreNosMomentosBuridogsMock.ts
@@ -0,0 +1,37 @@
+export const sobreNosMomentosBuridogsMock = [
+    {
+        ano: "2022",
+        imgSrc: "sobre-nos-homenagem-camara-1.jpg",
+        altText: "Homenagem Buri Dogs na Câmara de Belo Horizonte",
+        descricao:
+            "Em Março de 2022, o Buri Dogs foi homenageado na Câmara Municipal de Belo Horizonte. Juntamente com o vereador Miltinho, as voluntárias receberam o diploma de honra ao mérito pelo trabalho de assistência aos cãeszinhos abandonados no bairro Buritis.",
+    },
+    {
+        ano: "2022",
+        imgSrc: "sobre-nos-homenagem-camara-2.jpg",
+        altText: "Homenagem Buri Dogs na Câmara de Belo Horizonte",
+        descricao:
+            "A voluntária Chris Simões recebendo o diploma de honra ao mérito na Câmara Municipal de Belo Horizonte.",
+    },
+    {
+        ano: "2022",
+        imgSrc: "sobre-nos-homenagem-camara-3.jpg",
+        altText: "Homenagem Buri Dogs na Câmara de Belo Horizonte",
+        descricao:
+            "A voluntária Fernanda Castro recebendo o diploma de honra ao mérito na Câmara Municipal de Belo Horizonte.",
+    },
+    {
+        ano: "2022",
+        imgSrc: "sobre-nos-homenagem-camara-4.jpg",
+        altText: "Homenagem Buri Dogs na Câmara de Belo Horizonte",
+        descricao:
+            "Momentos antes de receber a homenagem, as voluntárias do Buri Dogs e o vereador Miltinho na Câmara Municipal de Belo Horizonte.",
+    },
+    {
+        ano: "2022",
+        imgSrc: "sobre-nos-homenagem-camara-5.jpg",
+        altText: "Homenagem Buri Dogs na Câmara de Belo Horizonte",
+        descricao:
+            "Instantes depois de receber a homenagem, as voluntárias do Buri Dogs e o vereador Miltinho registram o momento na Câmara Municipal de Belo Horizonte.",
+    },
+];
