ae69162fed9ecb46657565bec108f8c22cf4b0aa
Diogo Almazan <diogodpa@outlook.com>
2023-12-26 19:15:12 -0300

    Fixed send email adoption function name

--- src/services/azure-function/send-email-adocao/send-email-function-adocao-form.ts
+++ src/services/azure-function/send-email-adocao/send-email-function-adocao-form.ts
@@ -4,7 +4,7 @@ import { convertDataToTemplate } from "./send-email-function-adocao-template";
 export async function sendEmailFunctionAdocaoForm(adocaoData: IAdocaoPOSTRequestForm) {
     const bodyTemplate = convertDataToTemplate(adocaoData);
     const response = await fetch(
-        `https://serverless-email-buridogs.azurewebsites.net/api/SendEmailFunction?code=${process.env.NEXT_PUBLIC_AZURE_FUNCTION_KEY_SEND_EMAIL_FUNCTION_ADOCAO_FORM}`,
+        `https://serverless-email-buridogs.azurewebsites.net/api/SendEmailFunctionAdocaoForm?code=${process.env.NEXT_PUBLIC_AZURE_FUNCTION_KEY_SEND_EMAIL_FUNCTION_ADOCAO_FORM}`,
         {
             method: "POST",
             body: JSON.stringify({ Body: bodyTemplate }),
