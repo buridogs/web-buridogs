d69e84b906c0caf9dd1b506c0315d452dd4fa906
Diogo Almazan <diogodpa@outlook.com>
2024-01-04 20:53:48 -0300

    Volunteers component

---
+++ src/components/Footer/Volunteers/VolunteersCardWrapper.tsx
@@ -0,0 +1,37 @@
+import { volunteersWithHisSection } from "@/mock/volunteersMock";
+import { VolunteersCard } from "./VolunteersCard";
+import { VolunteerSituationEnum } from "@/interfaces/volunteersInterfaces";
+
+export function VolunteersCardWrapper() {
+    return (
+        <div className="w-[80%] mt-3 flex flex-col items-center">
+            {volunteersWithHisSection.map((volunteerSection) =>
+                volunteerSection.situation === VolunteerSituationEnum.origin ? (
+                    <VolunteersCard
+                        key={volunteerSection.volunteers[0].name}
+                        volunteer={volunteerSection.volunteers[0]}
+                    />
+                ) : (
+                    <div
+                        key={volunteerSection.situationDescription}
+                        className="w-full flex flex-col items-start mt-4"
+                    >
+                        <p className="text-gray-500 text-sm">
+                            {volunteerSection.situationDescription}
+                        </p>
+                        <div className="w-full flex justify-start gap-2 flex-wrap mt-2 pb-3">
+                            {volunteerSection.volunteers.map((volunteer) => (
+                                <VolunteersCard
+                                    key={volunteer.name}
+                                    volunteer={volunteer}
+                                    isContributor
+                                />
+                            ))}
+                        </div>
+                        <div className="w-3/4 h-1 m-[0_auto] border-b border-gray-300" />
+                    </div>
+                )
+            )}
+        </div>
+    );
+}
