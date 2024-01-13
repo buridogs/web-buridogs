4abd0ab7f4c768a3eb3276de21e3f661a6b0d131
joaolotjr <joaolotjr@gmail.com>
2024-01-13 14:29:50 -0300

    Testes de build

--- src/services/azure-function/send-email-adocao/send-email-function-adocao-template.ts
+++ src/services/azure-function/send-email-adocao/send-email-function-adocao-template.ts
@@ -40,40 +40,38 @@ export const convertDataToTemplate = (adocaoData: IAdocaoPOSTRequestForm) => {
 
     return `
     <html lang="pt-BR">
-      <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Formulário de Adoção</title>      
-      </head>
-      
-      <body style="font-family: 'Arial', sans-serif; margin: 8px; padding: 8px ; background-color: #ef7e0740;">
-        <div style = "max-width: 600px; margin: 40px auto; background-color: #ffffff; padding: 20px; border-radius: 8px;
-        box-shadow: 0 0 10px #0000001a;">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Adoção</title>      
+    </head>
+    
+    <body >
+      <div >
 
-          <h1 style="color: #EF7E07; text-align: center;">Formulário de Adoção Preenchido</h1>    
-          <h2 style = "color: #303E46; margin-bottom: 20px; font-size: 17px;">📆 Data de envio: ${formatDatetimePTBR(new Date().toISOString())}</h2>        
-          <h2 style = "color: #303E46; margin-bottom: 20px; font-size: 17px; ">🐶 Cachorro interessado: ${adocaoData.nomeCachorroAdocao}</h2> 
-          <p style = "color: #303E46; line-height: 1.6; font-weight: bold; padding-left: 26px"> Abaixo estão os resultados do formulário de adoção:</p>
+        <h1 >Formulário de Adoção Preenchido</h1>    
+        <h2 >📆 Data de envio: ${formatDatetimePTBR(new Date().toISOString())}</h2>        
+        <h2 >🐶 Cachorro interessado: ${adocaoData.nomeCachorroAdocao}</h2> 
+        <p > Abaixo estão os resultados do formulário de adoção:</p>
 
-          <ul>
-          ${Object.entries(keyLabels).reduce(
-              (acm: any, kl: any) =>
-                  acm +
-                  `<li style = "margin: 10px;">
-                      <strong>${kl[1]} : <span style = "border-bottom: 2px solid #303e464d;">${renderFormattedAwnser(
-                          adocaoData[kl[0] as keyof IAdocaoPOSTRequestForm] as string
-                      )}<span></strong>                    
-                  </li>\n`,
-              ""
-          )}
-          </ul>
-          <p style = "background-color: #303e4626; color: #131B20; text-align: center; max-width: 300px;
-          font-size: 10px; border-radius: 8px; margin: 24px auto;">
-              Caso esse mail contenha algum erro,<br>
-              entrar em contato com a equipe de desenvolvimento.
-          </p>
-        </div>
-      </body>    
-    </html>`;
+        <ul>
+        ${Object.entries(keyLabels).reduce(
+            (acm: any, kl: any) =>
+                acm +
+                `<li >
+                    <strong>${kl[1]} : <span >${renderFormattedAwnser(
+                        adocaoData[kl[0] as keyof IAdocaoPOSTRequestForm] as string
+                    )}<span></strong>                    
+                </li>\n`,
+            ""
+        )}
+        </ul>
+        <p >
+            Caso esse mail contenha algum erro,<br>
+            entrar em contato com a equipe de desenvolvimento.
+        </p>
+      </div>
+    </body>    
+  </html>`;
 };
--- src/services/azure-function/send-email-contato/send-email-function-contato-template.ts
+++ src/services/azure-function/send-email-contato/send-email-function-contato-template.ts
@@ -38,21 +38,20 @@ export const convertDataToTemplateContato = (contatoData: IContatoFormData) => {
       <title>Formulário de Contato</title>      
       </head>
 
-      <body style="font-family: 'Arial', sans-serif; margin: 8px; padding: 8px ; background-color: #ef7e0740;">
+      <body >
 
-        <div style = "max-width: 600px; margin: 40px auto; background-color: #ffffff; padding: 20px; border-radius: 8px;
-        box-shadow: 0 0 10px #0000001a;">
+        <div >
 
-          <h1 style="color: #EF7E07; text-align: center;">Formulário de Contato</h1>    
-          <h2 style = "color: #303E46; margin-bottom: 20px; font-size: 17px;">📆 Data de envio: ${formatDatetimePTBR(new Date().toISOString())}</h2>        
-          <p style = "color: #303E46; line-height: 1.6; font-weight: bold; padding-left: 26px"> Abaixo estão os dados do formulario de contato:</p>
+          <h1 >Formulário de Contato</h1>    
+          <h2 >📆 Data de envio: ${formatDatetimePTBR(new Date().toISOString())}</h2>        
+          <p > Abaixo estão os dados do formulario de contato:</p>
 
           <ul>
           ${Object.entries(keyLabels).reduce(
               (acm: any, kl: any) =>
                   acm +
-                  `<li style = "margin: 10px;">
-                      <strong>${kl[1]} : <span style = "border-bottom: 2px solid #303e464d";>${renderFormattedAwnser(
+                  `<li >
+                      <strong>${kl[1]} : <span ;>${renderFormattedAwnser(
                           contatoData[kl[0] as keyof IContatoFormData] as string
                       )}<span></strong>
                   </li>\n`,
@@ -60,8 +59,7 @@ export const convertDataToTemplateContato = (contatoData: IContatoFormData) => {
           )}
           </ul>
 
-          <p style = "background-color: #303e4626; color: #131B20; text-align: center; max-width: 300px;
-          font-size: 10px; border-radius: 8px; margin: 24px auto;">
+          <p >
             Caso esse mail contenha algum erro,<br>
             entrar em contato com a equipe de desenvolvimento.
           </p>
