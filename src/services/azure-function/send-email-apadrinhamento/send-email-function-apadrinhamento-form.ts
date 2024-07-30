06b81b356d92768cca4b2d4c3966b43df22dabb8
Diogo Almazan <diogodpa@outlook.com>
2024-07-29 21:34:18 -0300

    Changed function name for Apadrinhamento

--- src/services/azure-function/send-email-apadrinhamento/send-email-function-apadrinhamento-form.ts
+++ src/services/azure-function/send-email-apadrinhamento/send-email-function-apadrinhamento-form.ts
@@ -1,8 +1,8 @@
 import { IApadrinhamentoPOSTRequestForm } from "@/components/app/apadrinhamento/ApadrinhamentoTypes";
-import { convertDataToTemplate } from "./send-email-function-apadrinhamento-template";
+import { convertDataToTemplateApadrinhamento } from "./send-email-function-apadrinhamento-template";
 
 export async function sendEmailFunctionApadrinhamentoForm(apadrinhamentoData: IApadrinhamentoPOSTRequestForm) {
-    const bodyTemplate = convertDataToTemplate(apadrinhamentoData);
+    const bodyTemplate = convertDataToTemplateApadrinhamento(apadrinhamentoData);
     const response = await fetch(
         `https://serverless-email-buridogs.azurewebsites.net/api/SendEmailFunctionApadrinhamentoForm?code=${process.env.NEXT_PUBLIC_AZURE_FUNCTION_KEY_SEND_EMAIL_FUNCTION_APADRINHAMENTO_FORM}`,
         {
