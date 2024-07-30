0ca02c4a38b3613d116668057275202fd52d97b2
Diogo Almazan <diogodpa@outlook.com>
2024-07-29 21:24:35 -0300

    Added guidance text and tips for Apadrinhamento

--- src/components/app/apadrinhamento/ApadrinhamentoInstrucoes.tsx
+++ src/components/app/apadrinhamento/ApadrinhamentoInstrucoes.tsx
@@ -2,6 +2,16 @@ import { generateImgURL } from "@/utils/methods";
 import Image from "next/image";
 
 export function ApadrinhamentoInstrucoes() {
+    const instrucoes = [
+        "Mantenha-se Informado: Acompanhe as atualizações fornecidos pelo Buridogs nas redes sociais. Isso ajuda você a se manter conectado e informado sobre o progresso do cão que você apadrinha.",
+        "Comunique-se: Entre em contato com a organização se tiver alguma dúvida ou preocupação sobre seu apadrinhamento ou o bem-estar do cão.",
+        "Seja Paciente: Alguns cães podem demorar mais para se recuperar ou se ajustar. Seu apoio é crucial nesses momentos, então mantenha-se paciente e compreensivo.",
+        "Envolva-se: Participe de eventos e atividades organizados pelo Buridogs. É uma ótima maneira de se engajar com a comunidade e apoiar outros cães também.",
+        "Considere o Voluntariado: Se você tiver tempo, considere ser voluntário no abrigo. Sua ajuda prática pode fazer uma grande diferença.",
+        "Incentive a Adoção: Se conhecer alguém que esteja procurando adotar um cão, compartilhe informações sobre o abrigo e os cães disponíveis para adoção.",
+        "Use as Redes Sociais: Compartilhe atualizações e histórias sobre o cão que você patrocina nas redes sociais para aumentar a conscientização e atrair mais padrinhos(as).",
+    ];
+
     return (
         <section className="flex flex-col-reverse items-center lg:flex-row">
             <div className="flex flex-col items-center md:items-start">
@@ -12,23 +22,22 @@ export function ApadrinhamentoInstrucoes() {
                     Entenda como funciona o apadrinhamento
                 </h2>
                 <p className="text-gray-400 text-xl font-medium leading-6 text-left">
-                    Para adotar um cãozinho é bastante simples. Você escolhe o animal que tem as
-                    características que você deseja. Opta por ele clicando no botão &quot;Quero
-                    adotar esse&quot;. Você será direcionado para um formulário com perguntas sobre
-                    as expectativas e como será o ambiente em que o cão viverá. Vamos analisar se
-                    suas demandas combinam com o perfil do animal escolhido.
+                    Ao se tornar um padrinho(a), você está fazendo uma diferença significativa na
+                    vida de cães necessitados. Seu apoio ajuda a fornecer alimentação, cuidados
+                    médicos, abrigo e o amor que eles merecem. Aqui está um guia para ajudá-lo a
+                    entender como funciona o processo de patrocínio e como você pode se envolver.
                 </p>
                 <strong className="text-gray-100 text-lg uppercase my-6">
-                    Dicas para quem adota um cãozinho
+                    Dicas para quem apadrinha um cãozinho
                 </strong>
                 <ul className="list-disc [&>li+li]:mt-4 ml-4">
-                    <li className="text-gray-400 text-base font-medium">
-                        Não há mistério na adoção de um cãozinho, a regra é ser amoroso e paciente.
-                    </li>
+                    {instrucoes.map((instruction) => (
+                        <li className="text-gray-400 text-base font-medium">{instruction}</li>
+                    ))}
                 </ul>
             </div>
             <Image
-                src={generateImgURL("sobre-nos-foto-equipe-2.jpg")}
+                src={generateImgURL("sobre-nos-homenagem-camara-5.jpg")}
                 alt="Equipe do Buridogs"
                 width={300}
                 height={300}
