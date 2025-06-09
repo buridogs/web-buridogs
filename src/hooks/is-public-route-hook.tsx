d704a2658ad1257382a4d307e5a5c238ce39893a
Diogo Almazan <diogodpa@outlook.com>
2025-06-09 19:06:25 -0300

    Fixed header appearance

--- src/hooks/is-public-route-hook.tsx
+++ src/hooks/is-public-route-hook.tsx
@@ -6,7 +6,7 @@ export const isPublicRouteHook = (): boolean => {
     const pathname = usePathname();
 
     const isPublicRoute = useMemo(() => {
-        return Object.values(PublicRoutes).some((route) => pathname.startsWith(route));
+        return Object.values(PublicRoutes).some((route) => pathname === route);
     }, [pathname]);
 
     return isPublicRoute;
