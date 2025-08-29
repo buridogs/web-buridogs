e3cce6693d0415c9054da0b0652b8082fd230e66
Diogo Almazan <diogodpa@outlook.com>
2025-08-29 19:50:00 -0300

    Refactor Sentry configuration in next.config.js to streamline options and reduce build size by hiding source maps and disabling unnecessary server function instrumentation.

--- next.config.js
+++ next.config.js
@@ -36,14 +36,11 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-
 // Injected content via Sentry wizard below
 
 const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withSentryConfig(
-  module.exports,
-  {
+module.exports = withSentryConfig(module.exports, {
     // For all available options, see:
     // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
@@ -57,13 +54,11 @@ module.exports = withSentryConfig(
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
     // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
 
     // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
     // This can increase your server load as well as your hosting bill.
     // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
     // side errors will fail.
-    tunnelRoute: "/monitoring",
 
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
@@ -72,6 +67,8 @@ module.exports = withSentryConfig(
     // See the following for more information:
     // https://docs.sentry.io/product/crons/
     // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  }
-);
+
+    // Reduz tamanho do build
+    hideSourceMaps: true,
+    autoInstrumentServerFunctions: false, // Não necessário para Static Web App
+});
